999dbe342be376326b8274bbd148c414dc2cd514
Matthew Unrath <harkathmaker@gmail.com>
2016-11-25 23:57:16 -0800

    Add support for slash command

--- src/main.py
+++ src/main.py
@@ -4,7 +4,12 @@ import sys
 import getopt
 
 from slack.monitor import monitor
+from slack.slackslashhandler import SlackSlashHandler
 from cardrepository import CardRepository
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
 
 
 def initialize_logging(log_level):
@@ -25,7 +30,26 @@ def print_help():
     print('{0} [ -L|--log_level=[INFO|WARNING|ERROR] ]'.format(sys.argv[0]))
 
 
-def main():
+@app.route('/', methods=['POST'])
+def serve_matching_card():
+    print(request.form)
+    # token=gIkuvaNzQIHg97ATvDxqgjtO
+    # team_id=T0001
+    # team_domain=example
+    # channel_id=C2147483705
+    # channel_name=test
+    # user_id=U2147483697
+    # user_name=Steve
+    # command=/weather
+    # text=94070
+    # response_url=https://hooks.slack.com/commands/1234/5678
+    for key in request.form:
+        print("Key: "+key)
+        print(request.form[key])
+    #return 'Hello world'
+    return handler.get_matching_card(request.form['text'])
+
+if __name__ == "__main__":
     log_level = "WARNING"
     try:
         opts, args = getopt.getopt(sys.argv[1:],"hL:",["log_level="])
@@ -45,10 +69,12 @@ def main():
     logging.warning("Starting application...")
 
     repo = CardRepository()
-    monitor(repo)
+    #client = SlackClient(token)
+    handler = SlackSlashHandler(repo)
+
+    #monitor(repo)
+    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
 
     logging.warning("Exiting application...")
 
 
-if __name__ == "__main__":
-    main()
